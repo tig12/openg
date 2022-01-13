804d0fe05cd5df161a0b9052f90e77706684eb4f
Thierry <tigraff@gmail.com>
2022-01-13 21:17:19 +0100

    Finish pagination in page issues

--- src/control/issue.go
+++ src/control/issue.go
@@ -1,6 +1,7 @@
 package control
 
 import (
+    "math"
     "strconv"
 	"net/http"
 	"openg.local/openg/ctxt"
@@ -15,6 +16,10 @@ type detailsIssues struct {
 	WD_ENTITY_BASE_URL string
 	GroupSlugNames     map[string]string
 	Slug_Name          map[string]string
+	Pages              []int
+	CurrentPage        int
+	NextPage           int
+	PrevPage           int
 }
 
 /**
@@ -29,12 +34,17 @@ func ShowIssues(ctx *ctxt.Context, w http.ResponseWriter, r *http.Request) error
 		page, _ = strconv.Atoi(strPage) // error useless as routing imposes [1-9][0-9]*
 	}
 
-	issues, err := model.GetIssues(ctx.Config.RestURL, page, ctx.Config.NbPerPage)
-	
+	issues, pagemax, err := model.GetIssues(ctx.Config.RestURL, page, ctx.Config.NbPerPage)
 	if err != nil {
 		return err
 	}
-
+	var pages []int
+	for i := 1; i <= pagemax; i++ {
+		pages = append(pages, i)
+	}
+	nextPage := int(math.Min(float64(pagemax), float64(page+1)))
+	prevPage := int(math.Max(1, float64(page-1)))
+	
 	// to show the occus of a person
 	groupSlugNames, err := model.GetGroupSlugNames(ctx.Config.RestURL)
 	if err != nil {
@@ -51,6 +61,10 @@ func ShowIssues(ctx *ctxt.Context, w http.ResponseWriter, r *http.Request) error
 			DownloadBase:       ctx.Config.Paths.Downloads,
 			GroupSlugNames:     groupSlugNames,
 			WD_ENTITY_BASE_URL: model.WD_ENTITY_BASE_URL,
+			Pages:              pages,
+			CurrentPage:        page,
+			PrevPage:           prevPage,
+			NextPage:           nextPage,
 		},
 	}
 	return nil
--- src/model/issue.go
+++ src/model/issue.go
@@ -7,6 +7,8 @@
 package model
 
 import (
+    "math"
+    "strconv"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -28,34 +30,58 @@ type Issue struct {
     Returns all issues for a given page, sorted by name.
     @param  page    Current page of the issues to load
     @param  limit   Nb of members to fetch
+    @return issues
+            pagemax Max nb of pages (used for navigation)
 **/
-func GetIssues(restURL string, page, limit int) (issues []*Issue, err error) {
-
-/* 
-    n:= GetNbIssues(restURL)
-	pagemax := int(math.Ceil(float64(n) / float64(limit)))
-	group.NPages = pagemax
+func GetIssues(restURL string, page, limit int) (issues []*Issue, pagemax int, err error) {
+    
+    nIssues, err := GetNbIssues(restURL)
+	if err != nil {
+		return nil, 0, werr.Wrapf(err, "Cannot compute nIssues")
+	}
+	
+	pagemax = int(math.Ceil(float64(nIssues) / float64(limit)))
 	if page > pagemax {
 		page = pagemax
 	}
 	offset := (page - 1) * limit
-*/
 	
-	url := restURL + "/api_issue"
+	url := restURL + "/api_issue" +
+		"?limit=" + strconv.Itoa(limit) +
+		"&offset=" + strconv.Itoa(offset)
 	response, err := http.Get(url)
 	if err != nil {
-		return nil, werr.Wrapf(err, "Error calling postgres API: "+url)
+		return nil, 0, werr.Wrapf(err, "Error calling postgres API: "+url)
 	}
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, werr.Wrapf(err, "Error decoding issues data")
+		return nil, 0, werr.Wrapf(err, "Error decoding issues data")
 	}
 	if err = json.Unmarshal(responseData, &issues); err != nil {
-		return nil, werr.Wrapf(err, "Error json Unmarshal issues data\n"+string(responseData)+"\n")
+		return nil, 0, werr.Wrapf(err, "Error json Unmarshal issues data\n"+string(responseData)+"\n")
 	}
-	return issues, nil
+	return issues, pagemax, nil
 }
 
-func GetNbIssues(restURL string) int {
-    return 261 // TODO 
+func GetNbIssues(restURL string) (int, error) {
+	url := restURL + "/stats?select=n_issues"
+    
+	response, err := http.Get(url)
+	if err != nil {
+		return 0, werr.Wrapf(err, "Error calling postgrest API: \n"+url)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return 0, werr.Wrapf(err, "Error decoding data containing nb issues")
+	}
+	
+	type Info map[string]int
+	tmp := []Info{}
+	if err = json.Unmarshal(responseData, &tmp); err != nil {
+		return 0, werr.Wrapf(err, "Error json Unmarshal nb issues\n"+string(responseData)+"\n")
+	}
+	if len(tmp) == 0 {
+		return 0, werr.Wrapf(err, "EMPTY nb issues - need to be initialized")
+	}
+	return tmp[0]["n_issues"], nil
 }
