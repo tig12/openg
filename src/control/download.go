769764cf56973d8ce8352a77dbc69d8f1a0a9aa2
Thierry Graff <thierry@conjecto.com>
2022-11-16 07:58:48 +0100

    Continue download form, start controller

--- src/control/download.go
+++ src/control/download.go
@@ -7,22 +7,13 @@ import (
 	"net/http"
 	"openg.local/openg/ctxt"
 	"openg.local/openg/model"
+	"strings"
 	"fmt"
 )
 
 // To display the download form
 type detailsDownloadForm struct {
 	Occupations []*model.Group
-}
-
-// To process the download form
-type downloadFormData struct {
-    What            string
-    OnlyTimed       bool
-    // output format
-    NamesFirstLine  bool
-    Separator       string
-    DateFormat      string
 }
 
 func ShowDownloads2(ctx *ctxt.Context, w http.ResponseWriter, r *http.Request) error {
@@ -37,6 +28,11 @@ func ShowDownloads2(ctx *ctxt.Context, w http.ResponseWriter, r *http.Request) e
 			return err
 		}
 fmt.Printf("%+v\n",data)
+		csv, err := model.ExportGroup(data)
+fmt.Printf("%+v\n",csv)
+		if err != nil {
+			return err
+		}
 		//
 		redirect := "/downloads2"
 		ctx.Redirect = redirect
@@ -66,13 +62,29 @@ fmt.Printf("%+v\n",data)
 }
 
 
-func processDownloadForm(r *http.Request) (*downloadFormData, error) {
-	data := &downloadFormData{}
+func processDownloadForm(r *http.Request) (*model.DownloadFormData, error) {
+	data := &model.DownloadFormData{} // see model/export.go
 	var err error
 	if err = r.ParseForm(); err != nil {
 		return data, err
 	}
-fmt.Printf("%+v\n",data)
+	//
+	data.What = r.PostFormValue("radio-what")
+	if strings.HasPrefix(data.What, "occu-") {
+	    data.What = strings.Replace(data.What, "occu-", "", -1)
+	}
+	//
+	data.OnlyTimed = (r.PostFormValue("only-timed") == "only-timed")
+	//
+	data.FirstLineNames = (r.PostFormValue("first-line-names") == "first-line-names")
+	//
+    data.DateFormat = r.PostFormValue("radio-field-date")
+	//
+    if r.PostFormValue("radio-sep") == "comma" {
+        data.Separator = ","
+    } else {
+        data.Separator = ";"
+    }
 	//
 	return data, nil
 }
---
+++ src/model/export.go
@@ -0,0 +1,25 @@
+/******************************************************************************
+    Informations about acts (birth, marriage, death certificates).
+
+    @license    GPL
+    @history    2022-11-15 20:47:38+01:00, Thierry Graff : Creation
+********************************************************************************/
+package model
+
+import (
+)
+
+// To process the download form
+type DownloadFormData struct {
+    What            string      // slug of the group to download
+    OnlyTimed       bool
+    // output format
+    FirstLineNames  bool
+    Separator       string      // ";" or ","
+    DateFormat      string      // "date-iso" or "date-many-cols"
+}
+
+
+func ExportGroup(downloadFormData *DownloadFormData) (result string, err error) {
+    return "toto", nil
+}
